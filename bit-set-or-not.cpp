9683fb0f53b2b0502122d1c58ed35ed6187b1f86
dishan2843 <88586842+dishan2843@users.noreply.github.com>
2021-08-20 14:55:58 +0530

    bit_set_or_not
    https://www.hackerearth.com/practice/basic-programming/bit-manipulation/basics-of-bit-manipulation/tutorial/

---
+++ bit-set-or-not.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int i,n;
+    cin>>n>>i;
+    bool a = n & (n>>i);
+   
+   if(a == true)
+   {
+       cout<<"Yes, Bit is set"<<endl;
+   }
+   else
+   {
+       cout<<"No, Bit is not set"<<endl;
+   }
+	return 0;
+}
